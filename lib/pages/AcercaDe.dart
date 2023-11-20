2d355e982a8ac87aa268afcad2be3b8073d78202
Keira Yarethzy Beltrán Flores <camposyarethzy1821@gmail.com>
2023-11-20 11:37:06 -0600

    2.3
    45678ijn

---
+++ lib/pages/AcercaDe.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class AcercaDe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'img/keira.jpg',
+                    height: 150,
+                    width: 150,
+                  ),
+                  const Text(
+                    'KEIRA YARETHZY BELTRAN FLORES',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.purple),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  //Image.asset(
+                  //'img/ernesto.jpeg',
+                  //height: 150,
+                  //width: 150,
+                  // ),
+                  const Text(
+                    'ADELAYDA DESIRE',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.purple),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
